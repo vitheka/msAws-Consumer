046ec98ea877da881c5d2d523c51e7b8a632cd03
vitheka <vitorcoelhoft@gmail.com>
2024-07-06 15:19:50 -0300

    adjust folders and init ms-marketingStrategy

---
+++ ms-marketingStrategy/src/main/java/br/com/vitheka/marketingStrategy/MarketingStrategyApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitheka.marketingStrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingStrategyApplication.class, args);
+	}
+
+}
---
+++ ms-marketingStrategy/src/test/java/br/com/vitheka/marketingStrategy/MarketingStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitheka.marketingStrategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
